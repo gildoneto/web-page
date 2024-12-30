cf7e7a4d534936dd83c6942ccccb8773b051dfd4
Gildo Neto <32935771+gildoneto@users.noreply.github.com>
2024-12-30 16:25:54 -0300

    initialize React + TypeScript + Vite project with essential configurations and setup

---
+++ vitest.config.ts
@@ -0,0 +1,10 @@
+import { defineConfig } from 'vitest/config'
+
+// https://vite.dev/config/
+export default defineConfig({
+  test: {
+    environment: 'jsdom',
+    globals: true,
+    setupFiles: './src/setupTests.ts',
+  },
+})
